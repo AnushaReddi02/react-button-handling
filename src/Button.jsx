19f378e531f4a90ec6d0b1447f7c43eb764d4d03
AnushaReddi02 <anushareddi115@gmail.com>
2026-05-01 08:50:12 +0530

    Added double click event to the button

--- src/Button.jsx
+++ src/Button.jsx
@@ -8,11 +8,17 @@ function handleMouseOver(){
     console.log("Heyyyyy!!! It's over meeeeEEee... 3-)");
 }
 
+function handleDblClick(){
+    console.log("Heyyyyy!!! got slapped twiceeee...");
+}
+
+
 export default function Button(){
     return(
         <>
             <button className="btn" onClick={doSomething}>Click Me!</button>
             <p onMouseOver={handleMouseOver}>Every small step you take today is quietly shaping a stronger tomorrow. Growth doesn’t always shout; sometimes it happens in the silent moments of effort and persistence. Trust your journey, even when it feels slow, because progress is still progress. Keep going—your future self will thank you for not giving up.</p>
+            <button className="btn" onDoubleClick={handleDblClick}>Slap me Twice!</button>
         </>
     )
 }
