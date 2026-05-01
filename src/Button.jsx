2ea135b846b71599be88100a350b197ef35b3fcb
AnushaReddi02 <anushareddi115@gmail.com>
2026-05-01 08:32:07 +0530

    Added onMouseOver event to a paragraph element

--- src/Button.jsx
+++ src/Button.jsx
@@ -4,10 +4,15 @@ function doSomething(){
     console.log("Hey!!! I got clicked");
 }
 
+function handleMouseOver(){
+    console.log("Heyyyyy!!! It's over meeeeEEee... 3-)");
+}
+
 export default function Button(){
     return(
         <>
             <button className="btn" onClick={doSomething}>Click Me!</button>
+            <p onMouseOver={handleMouseOver}>Every small step you take today is quietly shaping a stronger tomorrow. Growth doesn’t always shout; sometimes it happens in the silent moments of effort and persistence. Trust your journey, even when it feels slow, because progress is still progress. Keep going—your future self will thank you for not giving up.</p>
         </>
     )
 }
